56b557b5b9b94004c46fd869cf967f4be250a9b0
Tyler Tabarovsky <tyler.tabarovsky7@gmail.com>
2025-04-06 11:29:41 -0400

    eslint

--- nowinvestment/next.config.ts
+++ nowinvestment/next.config.ts
@@ -1,9 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
     domains: ["images.unsplash.com"],
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
   },
 };
 
